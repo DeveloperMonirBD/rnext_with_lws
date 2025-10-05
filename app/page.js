3064c4f341b458b0b77608d24c5a9c2d13539e49
Monir <mrmonir0558@gmail.com>
2025-10-05 22:45:36 +0600

    Create a photofeed app that shows the list of photos on a page - Part 1

--- app/page.js
+++ app/page.js
@@ -1,6 +1,19 @@
 
 export default function Home() {
   return (
-    <h1 className="text-lg text-amber-400">Hello World !!</h1>
+      <>
+          <h1>Hello World !</h1>
+          <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi laboriosam deleniti ipsam voluptatem dicta sunt id natus! Quo laborum eligendi, culpa asperiores doloremque nesciunt
+              maiores repellat eos cumque explicabo, rem deleniti aperiam excepturi consequuntur ex debitis. Dolor nobis soluta exercitationem dignissimos, molestiae impedit ducimus alias praesentium
+              iste deleniti cum. Aperiam vero reiciendis unde ad, ducimus ipsa voluptate quibusdam molestiae nemo in labore suscipit sapiente quod illum provident magni facere tenetur nobis
+              repudiandae culpa! Sequi repellat vel quibusdam quos ipsa ut, consequuntur doloremque perspiciatis, vero excepturi blanditiis, quam facere praesentium cum eos rem omnis cumque culpa
+              quae. Ut, quis. Nam optio aliquam veritatis veniam nihil similique itaque architecto asperiores dolores inventore labore odit ipsam vitae ipsa adipisci, sint, sequi tenetur. Incidunt
+              veniam molestias debitis tenetur molestiae, quibusdam perferendis libero repudiandae veritatis vel. Illum voluptates illo eos accusantium atque deserunt, consectetur in natus placeat
+              dolore voluptate asperiores? Ex, dolor ipsam? Fuga, numquam! Aut error molestias maiores a ratione placeat! Iusto, alias quia sunt ipsam quas ipsum non illo asperiores nostrum a sit quae
+              et id, autem praesentium odit voluptates illum ducimus recusandae aspernatur dolore omnis! Voluptas vero quia debitis nesciunt. Sed aperiam nemo, possimus et cupiditate minus quidem
+              quibusdam! Nobis, impedit sapiente!
+          </p>
+      </>
   );
 }
